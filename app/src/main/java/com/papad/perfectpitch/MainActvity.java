63dd0d1bcae1faf5c9a268af36e57e64658c3a47
nikojpapa <nicholas.papadopoulos@comcast.net>
2016-04-29 14:25:16 -0400

    action bar buttons added

--- app/src/main/java/com/papad/perfectpitch/MainActvity.java
+++ app/src/main/java/com/papad/perfectpitch/MainActvity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -60,6 +61,13 @@ public class MainActvity extends AppCompatActivity implements GuessFrequencyFrag
 
 //        tabLayout.setOnTabSelectedListener();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     private void playWave() {
